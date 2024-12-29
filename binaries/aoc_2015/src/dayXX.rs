11cdccb1948847e92eb355f39930f4eb5add0909
Štěpán Beneš <benes.stepan@gmail.com>
2024-12-29 19:37:58 +0100

    Add template

---
+++ binaries/aoc_2015/src/dayXX.rs
@@ -0,0 +1,51 @@
+use solver::Solver;
+
+pub struct DayXXSolver {}
+
+impl DayXXSolver {
+    pub fn new(input: &'static str) -> Self {
+        DayXXSolver {}
+    }
+}
+
+impl Solver for DayXXSolver {
+    fn solve_part_one(&self) -> String {
+        "".to_string()
+    }
+
+    fn solve_part_two(&self) -> String {
+        "".to_string()
+    }
+
+    fn day_number(&self) -> usize {
+        0
+    }
+
+    fn description(&self) -> &'static str {
+        ""
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    mod part1 {
+        use super::*;
+
+        #[test]
+        fn test_1() {
+            let result = DayXXSolver::new("abc").solve_part_one();
+            assert_eq!(result, "0");
+        }
+    }
+
+    mod part2 {
+        use super::*;
+
+        #[test]
+        fn test_1() {
+            let result = DayXXSolver::new("abc").solve_part_two();
+            assert_eq!(result, "0");
+        }
+    }
+}
