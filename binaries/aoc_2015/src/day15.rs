cc0ee679f4a41376ae3f7d83d4220cafeebcad50
Štěpán Beneš <benes.stepan@gmail.com>
2025-01-05 00:05:19 +0100

    2015 day 15, part 2

--- binaries/aoc_2015/src/day15.rs
+++ binaries/aoc_2015/src/day15.rs
@@ -3,7 +3,7 @@ use solver::SolverBase;
 
 #[derive(Debug, Clone)]
 struct Ingredient {
-    name: &'static str,
+    _name: &'static str,
     capacity: i64,
     durability: i64,
     flavor: i64,
@@ -21,11 +21,11 @@ impl Solver {
         for line in input.lines() {
             let tokens: Vec<_> = line.split([':', ' ', ',']).collect();
             // Sprinkles: capacity 5, durability -1, flavor 0, texture 0, calories 5
-            if let [name, "", "capacity", capacity, "", "durability", durability, "", "flavor", flavor, "", "texture", texture, "", "calories", calories] =
+            if let [_name, "", "capacity", capacity, "", "durability", durability, "", "flavor", flavor, "", "texture", texture, "", "calories", calories] =
                 &tokens[..]
             {
                 ingredients.push(Ingredient {
-                    name,
+                    _name,
                     capacity: capacity.parse().unwrap(),
                     durability: durability.parse().unwrap(),
                     flavor: flavor.parse().unwrap(),
@@ -37,17 +37,24 @@ impl Solver {
         Solver { ingredients }
     }
 
-    /// Function to calculate the score for a combination of items
-    fn score_function(items: &[(Ingredient, usize)]) -> i64 {
+    fn score_function(items: &[(Ingredient, usize)], required_calories: Option<i64>) -> i64 {
         let mut capacity_sum = 0;
         let mut durability_sum = 0;
         let mut flavor_sum = 0;
         let mut texture_sum = 0;
+        let mut calories_sum = 0;
         for (ingredient, teaspoons) in items.iter() {
             capacity_sum += ingredient.capacity * *teaspoons as i64;
             durability_sum += ingredient.durability * *teaspoons as i64;
             flavor_sum += ingredient.flavor * *teaspoons as i64;
             texture_sum += ingredient.texture * *teaspoons as i64;
+            calories_sum += ingredient.calories * *teaspoons as i64;
+        }
+
+        if let Some(required_calories) = required_calories {
+            if calories_sum != required_calories {
+                return 0;
+            }
         }
 
         capacity_sum.max(0) * durability_sum.max(0) * flavor_sum.max(0) * texture_sum.max(0)
@@ -69,7 +76,7 @@ impl Solver {
         partitions
     }
 
-    fn maximize_score(&self) -> (i64, Vec<(Ingredient, usize)>) {
+    fn maximize_score(&self, required_calories: Option<i64>) -> (i64, Vec<(Ingredient, usize)>) {
         // All possible combinations of items (power set)
         let mut max_score = 0;
         let mut best_combination: Vec<(Ingredient, usize)> = vec![];
@@ -87,7 +94,8 @@ impl Solver {
                     .into_iter()
                     .zip(partition)
                     .collect::<Vec<_>>();
-                let score = Solver::score_function(combination_partition.as_slice());
+                let score =
+                    Solver::score_function(combination_partition.as_slice(), required_calories);
 
                 if score > max_score {
                     max_score = score;
@@ -102,13 +110,15 @@ impl Solver {
 
 impl SolverBase for Solver {
     fn solve_part_one(&self) -> String {
-        let (max_score, _best_combination) = self.maximize_score();
+        let (max_score, _best_combination) = self.maximize_score(None);
         //println!("{max_score:?}, {best_combination:?}");
         max_score.to_string()
     }
 
     fn solve_part_two(&self) -> String {
-        "".to_string()
+        let (max_score, _best_combination) = self.maximize_score(Some(500));
+        //println!("{max_score:?}, {best_combination:?}");
+        max_score.to_string()
     }
 
     fn day_number(&self) -> usize {
@@ -131,17 +141,21 @@ mod part1_tests {
 Cinnamon: capacity 2, durability 3, flavor -2, texture -1, calories 3",
         )
         .solve_part_one();
-        assert_eq!(result, "62842880x");
+        assert_eq!(result, "62842880");
     }
 }
 
-// #[cfg(test)]
-// mod part2_tests {
-//     use super::*;
+#[cfg(test)]
+mod part2_tests {
+    use super::*;
 
-//     #[test]
-//     fn test_1() {
-//         let result = Solver::new("abc").solve_part_two();
-//         assert_eq!(result, "0");
-//     }
-// }
+    #[test]
+    fn test_1() {
+        let result = Solver::new(
+            r"Butterscotch: capacity -1, durability -2, flavor 6, texture 3, calories 8
+Cinnamon: capacity 2, durability 3, flavor -2, texture -1, calories 3",
+        )
+        .solve_part_two();
+        assert_eq!(result, "57600000");
+    }
+}
