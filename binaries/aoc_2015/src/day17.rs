5015e1bb2c4d84345f817bc91992694881888bc0
Štěpán Beneš <benes.stepan@gmail.com>
2025-01-05 18:12:49 +0100

    2015 Day 17, part 2

--- binaries/aoc_2015/src/day17.rs
+++ binaries/aoc_2015/src/day17.rs
@@ -14,10 +14,30 @@ impl Solver {
         }
     }
 
-    fn count_combinations(containers: Vec<i32>, volume: i32) -> i32 {
-        fn backtrack(containers: &Vec<i32>, index: usize, current_sum: i32, target: i32) -> i32 {
+    fn count_combinations(
+        containers: Vec<i32>,
+        volume: i32,
+        container_count_limit: Option<usize>,
+    ) -> (i32, usize) {
+        fn backtrack(
+            containers: &Vec<i32>,
+            index: usize,
+            current_sum: i32,
+            target: i32,
+            container_count: usize,
+            container_count_limit: Option<usize>,
+            min_countainer_count: &mut usize,
+        ) -> i32 {
+            if let Some(container_count_limit) = container_count_limit {
+                if container_count > container_count_limit {
+                    return 0;
+                }
+            }
             // Base case: current_sum equals target
             if current_sum == target {
+                if container_count < *min_countainer_count {
+                    *min_countainer_count = container_count;
+                }
                 return 1;
             }
             // Base case: current_sum exceeds target or index is out of bounds
@@ -31,28 +51,52 @@ impl Solver {
                 index + 1,
                 current_sum + containers[index],
                 target,
+                container_count + 1,
+                container_count_limit,
+                min_countainer_count,
             );
-            let exclude = backtrack(containers, index + 1, current_sum, target);
+            let exclude = backtrack(
+                containers,
+                index + 1,
+                current_sum,
+                target,
+                container_count,
+                container_count_limit,
+                min_countainer_count,
+            );
 
             include + exclude
         }
+        let mut min_countainer_count = containers.len();
 
-        // Sort canisters to handle duplicates consistently
-        //let mut sorted_containers = containers;
-        //sorted_containers.sort();
-        //backtrack(&sorted_containers, 0, 0, volume)
-        backtrack(&containers, 0, 0, volume)
+        let count = backtrack(
+            &containers,
+            0,
+            0,
+            volume,
+            0,
+            container_count_limit,
+            &mut min_countainer_count,
+        );
+        (count, min_countainer_count)
     }
 }
 
 impl SolverBase for Solver {
     fn solve_part_one(&self) -> String {
-        let count = Solver::count_combinations(self.containers.clone(), self.volume);
+        let (count, _) = Solver::count_combinations(self.containers.clone(), self.volume, None);
         count.to_string()
     }
 
     fn solve_part_two(&self) -> String {
-        "".to_string()
+        let (_, min_container_count) =
+            Solver::count_combinations(self.containers.clone(), self.volume, None);
+        let (count, _) = Solver::count_combinations(
+            self.containers.clone(),
+            self.volume,
+            Some(min_container_count),
+        );
+        count.to_string()
     }
 
     fn day_number(&self) -> usize {
