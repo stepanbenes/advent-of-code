d79621667821ec8f5cd9e025689e0ea09b2d4ad1
Štěpán Beneš <benes.stepan@gmail.com>
2025-01-27 15:17:51 +0100

    2016, Day 5, part 2

--- binaries/aoc_2016/src/day05.rs
+++ binaries/aoc_2016/src/day05.rs
@@ -9,17 +9,43 @@ impl Solver {
         Solver { input }
     }
 
-    fn get_first_char_of_hashed_word_with_index(word: &str, start_index: u32) -> (char, u32) {
+    fn get_first_char_of_hashed_word_with_index(
+        word: &str,
+        start_index: u32,
+    ) -> (char, char, Option<usize>, u32) {
         let mut index = start_index;
         loop {
             let test = format!("{word}{index}");
             let hash = md5::compute(test);
             let hash_hex = format!("{:x}", hash);
             if hash_hex.starts_with("00000") {
-                return (hash_hex.chars().nth(5).unwrap(), index);
+                let c1 = hash_hex.chars().nth(5).unwrap();
+                let c2 = hash_hex.chars().nth(6).unwrap();
+                let mut position = None;
+                if let Ok(x) = hash_hex[5..6].parse::<usize>() {
+                    if x <= 7 {
+                        position = Some(x);
+                    }
+                }
+                return (c1, c2, position, index);
             }
             index += 1;
         }
+    }
+
+    fn set_char_at_index(s: &str, index: usize, new_char: char) -> String {
+        // Convert the string into a vector of characters
+        let mut chars: Vec<char> = s.chars().collect();
+
+        // Replace the character at the specified index
+        if index < chars.len() {
+            chars[index] = new_char;
+        } else {
+            panic!("Index out of bounds");
+        }
+
+        // Reconstruct the string from the characters
+        chars.into_iter().collect()
     }
 }
 
@@ -28,7 +54,7 @@ impl SolverBase for Solver {
         let mut index = 0;
         let mut password = String::new();
         for _ in 0..8 {
-            let (c, current_index) =
+            let (c, _c2, _position, current_index) =
                 Solver::get_first_char_of_hashed_word_with_index(self.input, index);
             password.push(c);
             index = current_index + 1;
@@ -37,7 +63,23 @@ impl SolverBase for Solver {
     }
 
     fn solve_part_two(&self) -> String {
-        "".to_string()
+        let mut index = 0;
+        let mut password = "________".to_owned();
+        loop {
+            let (_c1, c2, position, current_index) =
+                Solver::get_first_char_of_hashed_word_with_index(self.input, index);
+            if let Some(position) = position {
+                if password.chars().nth(position).unwrap() == '_' {
+                    password = Solver::set_char_at_index(&password, position, c2);
+                    println!("{password}");
+                    if password.chars().filter(|x| *x != '_').count() == 8 {
+                        break;
+                    }
+                }
+            }
+            index = current_index + 1;
+        }
+        password
     }
 
     fn day_number(&self) -> usize {
@@ -55,21 +97,21 @@ mod part1_tests {
 
     #[test]
     fn test_1() {
-        let (c, index) = Solver::get_first_char_of_hashed_word_with_index("abc", 0);
+        let (c, _, _, index) = Solver::get_first_char_of_hashed_word_with_index("abc", 0);
         assert_eq!(index, 3231929);
         assert_eq!(c, '1');
     }
 
     #[test]
     fn test_2() {
-        let (c, index) = Solver::get_first_char_of_hashed_word_with_index("abc", 5017308);
+        let (c, _, _, index) = Solver::get_first_char_of_hashed_word_with_index("abc", 5017308);
         assert_eq!(index, 5017308);
         assert_eq!(c, '8');
     }
 
     #[test]
     fn test_3() {
-        let (c, index) = Solver::get_first_char_of_hashed_word_with_index("abc", 5017308 + 1);
+        let (c, _, _, index) = Solver::get_first_char_of_hashed_word_with_index("abc", 5017308 + 1);
         assert_eq!(index, 5278568);
         assert_eq!(c, 'f');
     }
@@ -81,13 +123,13 @@ mod part1_tests {
     }
 }
 
-// #[cfg(test)]
-// mod part2_tests {
-//     use super::*;
+#[cfg(test)]
+mod part2_tests {
+    use super::*;
 
-//     #[test]
-//     fn test_1() {
-//         let result = Solver::new("abc").solve_part_two();
-//         assert_eq!(result, "0");
-//     }
-// }
+    #[test]
+    fn test_1() {
+        let result = Solver::new("abc").solve_part_two();
+        assert_eq!(result, "05ace8e3");
+    }
+}
