62387291e442dac493093a6c128571efae3c2e95
Štěpán Beneš <benes.stepan@gmail.com>
2025-02-19 11:55:22 +0100

    2017, day 02, part 2

--- binaries/aoc_2017/src/day01.rs
+++ binaries/aoc_2017/src/day01.rs
@@ -1,4 +1,3 @@
-use itertools::Itertools;
 use solver::SolverBase;
 
 pub struct Solver {
@@ -14,25 +13,37 @@ impl Solver {
                 .collect(),
         }
     }
-}
 
-impl SolverBase for Solver {
-    fn solve_part_one(&self) -> String {
+    fn get_next_index(&self, index: usize, offset: usize) -> usize {
+        (index + offset) % self.input.len()
+    }
+
+    fn solve(&self, offset: usize) -> u32 {
         let mut sum = 0;
         let mut updater = |a: u8, b: u8| {
             if a == b {
                 sum += a as u32;
             }
         };
-        for (&a, &b) in self.input.iter().tuple_windows() {
+        for index in 0..self.input.len() {
+            let next_index = self.get_next_index(index, offset);
+            let a = self.input[index];
+            let b = self.input[next_index];
             updater(a, b);
         }
-        updater(*self.input.last().unwrap(), *self.input.first().unwrap());
+        sum
+    }
+}
+
+impl SolverBase for Solver {
+    fn solve_part_one(&self) -> String {
+        let sum = self.solve(1);
         sum.to_string()
     }
 
     fn solve_part_two(&self) -> String {
-        "".to_string()
+        let sum = self.solve(self.input.len() / 2);
+        sum.to_string()
     }
 
     fn day_number(&self) -> usize {
@@ -70,5 +81,40 @@ mod part1_tests {
     fn test_4() {
         let result = Solver::new("91212129").solve_part_one();
         assert_eq!(result, "9");
+    }
+}
+
+#[cfg(test)]
+mod part2_tests {
+    use super::*;
+
+    #[test]
+    fn test_1() {
+        let result = Solver::new("1212").solve_part_two();
+        assert_eq!(result, "6");
+    }
+
+    #[test]
+    fn test_2() {
+        let result = Solver::new("1221").solve_part_two();
+        assert_eq!(result, "0");
+    }
+
+    #[test]
+    fn test_3() {
+        let result = Solver::new("123425").solve_part_two();
+        assert_eq!(result, "4");
+    }
+
+    #[test]
+    fn test_4() {
+        let result = Solver::new("123123").solve_part_two();
+        assert_eq!(result, "12");
+    }
+
+    #[test]
+    fn test_5() {
+        let result = Solver::new("12131415").solve_part_two();
+        assert_eq!(result, "4");
     }
 }
