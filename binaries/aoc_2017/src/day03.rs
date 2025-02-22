be46c7b158ca6dc9e1eb976e329091883176f19b
Štěpán Beneš <benes.stepan@gmail.com>
2025-02-22 16:43:52 +0100

    2017, Day 03, part 2

--- binaries/aoc_2017/src/day03.rs
+++ binaries/aoc_2017/src/day03.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use solver::SolverBase;
 
 pub struct Solver {
@@ -19,7 +21,7 @@ impl Solver {
     }
 
     fn get_coordinates(square_number: i32) -> (i32, i32) {
-        let mut direction = Direction::Left;
+        let mut direction = Direction::Right;
         let mut square: i32 = 1;
         let mut increment: i32 = 1;
         let mut x: i32 = 0;
@@ -61,6 +63,84 @@ impl Solver {
         }
         (x, y)
     }
+
+    fn get_first_value_larger_than(number: i32) -> i32 {
+        let mut direction = Direction::Right;
+        let mut increment: i32 = 1;
+        let mut x: i32 = 0;
+        let mut y: i32 = 0;
+        let mut map = HashMap::new();
+        map.insert((0, 0), 1);
+
+        fn get_sum_of_adjacent_squares(coords: &(i32, i32), map: &HashMap<(i32, i32), i32>) -> i32 {
+            let adjacent_squares = [
+                *map.get(&(coords.0 + 1, coords.1)).unwrap_or(&0),
+                *map.get(&(coords.0 + 1, coords.1 + 1)).unwrap_or(&0),
+                *map.get(&(coords.0, coords.1 + 1)).unwrap_or(&0),
+                *map.get(&(coords.0 - 1, coords.1 + 1)).unwrap_or(&0),
+                *map.get(&(coords.0 - 1, coords.1)).unwrap_or(&0),
+                *map.get(&(coords.0 - 1, coords.1 - 1)).unwrap_or(&0),
+                *map.get(&(coords.0, coords.1 - 1)).unwrap_or(&0),
+                *map.get(&(coords.0 + 1, coords.1 - 1)).unwrap_or(&0),
+            ];
+            adjacent_squares.iter().sum()
+        }
+
+        loop {
+            match direction {
+                Direction::Right => {
+                    for _ in 0..increment {
+                        x += 1;
+                        let coords = (x, y);
+                        let square_value = get_sum_of_adjacent_squares(&coords, &map);
+                        if square_value > number {
+                            return square_value;
+                        }
+                        map.insert(coords, square_value);
+                    }
+                    direction = Direction::Up;
+                }
+                Direction::Up => {
+                    for _ in 0..increment {
+                        y += 1;
+                        let coords = (x, y);
+                        let square_value = get_sum_of_adjacent_squares(&coords, &map);
+                        if square_value > number {
+                            return square_value;
+                        }
+                        map.insert(coords, square_value);
+                    }
+                    increment += 1;
+                    direction = Direction::Left;
+                }
+                Direction::Left => {
+                    for _ in 0..increment {
+                        x -= 1;
+                        let coords = (x, y);
+                        let square_value = get_sum_of_adjacent_squares(&coords, &map);
+                        if square_value > number {
+                            return square_value;
+                        }
+                        map.insert(coords, square_value);
+                    }
+                    direction = Direction::Down;
+                }
+                Direction::Down => {
+                    for _ in 0..increment {
+                        y -= 1;
+                        let coords = (x, y);
+                        let square_value = get_sum_of_adjacent_squares(&coords, &map);
+                        if square_value > number {
+                            return square_value;
+                        }
+                        map.insert(coords, square_value);
+                    }
+                    increment += 1;
+                    direction = Direction::Right;
+                }
+            }
+        }
+    }
 }
 
 impl SolverBase for Solver {
@@ -71,7 +151,8 @@ impl SolverBase for Solver {
     }
 
     fn solve_part_two(&self) -> String {
-        "".to_string()
+        let value = Solver::get_first_value_larger_than(self.square_number);
+        value.to_string()
     }
 
     fn day_number(&self) -> usize {
@@ -112,13 +193,25 @@ mod part1_tests {
     }
 }
 
-// #[cfg(test)]
-// mod part2_tests {
-//     use super::*;
+#[cfg(test)]
+mod part2_tests {
+    use super::*;
 
-//     #[test]
-//     fn test_1() {
-//         let result = Solver::new("abc").solve_part_two();
-//         assert_eq!(result, "0");
-//     }
-// }
+    #[test]
+    fn test_1() {
+        let result = Solver::get_first_value_larger_than(1);
+        assert_eq!(result, 2);
+    }
+
+    #[test]
+    fn test_2() {
+        let result = Solver::get_first_value_larger_than(70);
+        assert_eq!(result, 122);
+    }
+
+    #[test]
+    fn test_3() {
+        let result = Solver::get_first_value_larger_than(750);
+        assert_eq!(result, 806);
+    }
+}
