a709d9aa0459382c8a77d0798d3a5bd61ba5565d
Štěpán Beneš <benes.stepan@gmail.com>
2025-03-02 21:34:08 +0100

    2017, Day 11, part 1

--- binaries/aoc_2017/src/day11.rs
+++ binaries/aoc_2017/src/day11.rs
@@ -4,14 +4,14 @@ pub struct Solver {
     directions: Vec<Direction>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 enum Direction {
-    North,
-    Northeast,
-    Southeast,
-    South,
-    Southwest,
-    Northwest,
+    North = 0,
+    Northeast = 1,
+    Southeast = 2,
+    South = 3,
+    Southwest = 4,
+    Northwest = 5,
 }
 
 impl Solver {
@@ -35,33 +35,62 @@ impl Solver {
 
 impl SolverBase for Solver {
     fn solve_part_one(&self) -> String {
-        let mut x: i32 = 0;
-        let mut y: i32 = 0;
-        for direction in self.directions.iter() {
-            match direction {
-                Direction::North => y += 2,
-                Direction::Northeast => {
-                    x += 2;
-                    y += 1;
-                }
-                Direction::Southeast => {
-                    x += 2;
-                    y -= 1;
-                }
-                Direction::South => {
-                    y -= 2;
-                }
-                Direction::Southwest => {
-                    x -= 2;
-                    y -= 1;
-                }
-                Direction::Northwest => {
-                    x -= 2;
-                    y += 1;
+        let mut dir_table = [0u32; 6];
+
+        let get_triplet_to_merge = |table: &[u32; 6]| {
+            for center_index in 0..6 {
+                let left_index = if center_index == 0 {
+                    5
+                } else {
+                    center_index - 1
+                };
+                let right_index = if center_index == 5 {
+                    0
+                } else {
+                    center_index + 1
+                };
+                if table[left_index] > 0 && table[right_index] > 0 {
+                    return Some((left_index, center_index, right_index));
                 }
             }
+            None
+        };
+
+        let get_duplet_to_equalize = |table: &[u32; 6]| {
+            for index in 0..6 {
+                let mut other_index = index + 3;
+                if other_index > 5 {
+                    other_index -= 6;
+                }
+                if table[index] > 0 && table[other_index] > 0 {
+                    return Some((index, other_index));
+                }
+            }
+            None
+        };
+
+        for direction in self.directions.iter() {
+            dir_table[*direction as usize] += 1;
         }
-        "".to_string()
+
+        while let Some((left_index, center_index, right_index)) = get_triplet_to_merge(&dir_table) {
+            let left_value = dir_table[left_index];
+            let right_value = dir_table[right_index];
+            let min_value = left_value.min(right_value);
+            dir_table[left_index] -= min_value;
+            dir_table[right_index] -= min_value;
+            dir_table[center_index] += min_value;
+        }
+
+        while let Some((index, other_index)) = get_duplet_to_equalize(&dir_table) {
+            let value = dir_table[index];
+            let other_value = dir_table[other_index];
+            let min_value = value.min(other_value);
+            dir_table[index] -= min_value;
+            dir_table[other_index] -= min_value;
+        }
+
+        dir_table.iter().sum::<u32>().to_string()
     }
 
     fn solve_part_two(&self) -> String {
