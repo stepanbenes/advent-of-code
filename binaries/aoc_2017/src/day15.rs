bb90016bd5af47bc0e8ceb4fb4ed60c5b72e3868
Štěpán Beneš <benes.stepan@gmail.com>
2025-03-08 11:33:40 +0100

    2017, day 15, part 2

--- binaries/aoc_2017/src/day15.rs
+++ binaries/aoc_2017/src/day15.rs
@@ -31,6 +31,45 @@ impl Solver {
         }
         match_count
     }
+
+    fn get_matches_2(&self, count: usize) -> usize {
+        let mut match_count = 0;
+        let mut a = self.start_value_a;
+        let mut b = self.start_value_b;
+        let factor_a = 16807;
+        let factor_b = 48271;
+        let mod_n = 2147483647;
+        let mut get_next_a = move || {
+            loop {
+                a = (a * factor_a) % mod_n;
+                if a % 4 == 0 {
+                    break;
+                }
+            }
+            a
+        };
+        let mut get_next_b = move || {
+            loop {
+                b = (b * factor_b) % mod_n;
+                if b % 8 == 0 {
+                    break;
+                }
+            }
+            b
+        };
+        for _ in 0..count {
+            //a = (a * factor_a) % mod_n;
+            //b = (b * factor_b) % mod_n;
+            a = get_next_a();
+            b = get_next_b();
+            let a_low16 = a & (u16::MAX as u64);
+            let b_low16 = b & (u16::MAX as u64);
+            if a_low16 == b_low16 {
+                match_count += 1;
+            }
+        }
+        match_count
+    }
 }
 
 impl SolverBase for Solver {
@@ -40,7 +79,8 @@ impl SolverBase for Solver {
     }
 
     fn solve_part_two(&self) -> String {
-        "".to_string()
+        let match_count = self.get_matches_2(5_000_000);
+        match_count.to_string()
     }
 
     fn day_number(&self) -> usize {
@@ -63,13 +103,13 @@ mod part1_tests {
     }
 }
 
-// #[cfg(test)]
-// mod part2_tests {
-//     use super::*;
+#[cfg(test)]
+mod part2_tests {
+    use super::*;
 
-//     #[test]
-//     fn test_1() {
-//         let result = Solver::new("abc").solve_part_two();
-//         assert_eq!(result, "0");
-//     }
-// }
+    #[test]
+    fn test_1() {
+        let result = Solver::new(65, 8921).solve_part_two();
+        assert_eq!(result, "309");
+    }
+}
