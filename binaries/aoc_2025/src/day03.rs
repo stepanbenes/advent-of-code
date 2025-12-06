54d561b12e9e2f1a145535b96071f76fbdc45e96
Štěpán Beneš <benes.stepan@gmail.com>
2025-12-06 19:14:52 +0100

    2025 day03 part 2

--- binaries/aoc_2025/src/day03.rs
+++ binaries/aoc_2025/src/day03.rs
@@ -1,19 +1,23 @@
 use solver::SolverBase;
 
 pub struct Solver {
-    battery_joltage: Vec<Vec<u32>>,
+    battery_joltage: Vec<Vec<u64>>,
 }
 
 impl Solver {
     pub fn new(input: &'static str) -> Self {
         let battery_joltage = input
             .lines()
-            .map(|line| line.chars().map(|c| c.to_digit(10).unwrap()).collect())
+            .map(|line| {
+                line.chars()
+                    .map(|c| c.to_digit(10).unwrap() as u64)
+                    .collect()
+            })
             .collect();
         Solver { battery_joltage }
     }
 
-    fn find_largest_joltage(bank: &[u32]) -> u32 {
+    fn find_largest_joltage_two(bank: &[u64]) -> u64 {
         let (first_max_index, first_max_value) = bank[..bank.len() - 1]
             .iter()
             .enumerate()
@@ -22,19 +26,38 @@ impl Solver {
         let second_max_value = bank[first_max_index + 1..bank.len()].iter().max().unwrap();
         *first_max_value * 10 + *second_max_value
     }
+
+    fn find_largest_joltage_twelve(bank: &[u64]) -> u64 {
+        let mut joltage = 0;
+        let mut pivot = 0;
+        for i in (0..=11).rev() {
+            let (max_index, max_value) = bank[pivot..bank.len() - i]
+                .iter()
+                .enumerate()
+                .max_by(|a, b| a.1.cmp(b.1).then_with(|| b.0.cmp(&a.0)))
+                .unwrap();
+            joltage = joltage * 10 + max_value;
+            pivot += max_index + 1;
+        }
+        joltage
+    }
 }
 
 impl SolverBase for Solver {
     fn solve_part_one(&self) -> String {
         self.battery_joltage
             .iter()
-            .map(|bank| Solver::find_largest_joltage(bank))
-            .sum::<u32>()
+            .map(|bank| Solver::find_largest_joltage_two(bank))
+            .sum::<u64>()
             .to_string()
     }
 
     fn solve_part_two(&self) -> String {
-        "".to_string()
+        self.battery_joltage
+            .iter()
+            .map(|bank| Solver::find_largest_joltage_twelve(bank))
+            .sum::<u64>()
+            .to_string()
     }
 
     fn day_number(&self) -> usize {
@@ -63,13 +86,19 @@ mod part1_tests {
     }
 }
 
-// #[cfg(test)]
-// mod part2_tests {
-//     use super::*;
+#[cfg(test)]
+mod part2_tests {
+    use super::*;
 
-//     #[test]
-//     fn test_1() {
-//         let result = Solver::new("abc").solve_part_two();
-//         assert_eq!(result, "0");
-//     }
-// }
+    #[test]
+    fn test_1() {
+        let result = Solver::new(
+            r"987654321111111
+811111111111119
+234234234234278
+818181911112111",
+        )
+        .solve_part_two();
+        assert_eq!(result, "3121910778619");
+    }
+}
