a5b62994e208704392aca4778d93bc49108d06ee
Stepan Benes <benes.s@eplan.cz>
2025-12-12 12:28:43 +0100

    2025 Day06 part 2

--- binaries/aoc_2025/src/day06.rs
+++ binaries/aoc_2025/src/day06.rs
@@ -1,7 +1,8 @@
 use solver::SolverBase;
 
 pub struct Solver {
-    columns: Vec<Column>,
+    columns_part1: Vec<Column>,
+    columns_part2: Vec<Column>,
 }
 
 #[derive(Debug, Clone)]
@@ -40,7 +41,7 @@ impl Solver {
             }
         }
 
-        let mut columns = Vec::<Column>::new();
+        let mut columns_part1 = Vec::<Column>::new();
         for column_index in 0..operator_line.len() {
             let column = Column {
                 operator: operator_line[column_index],
@@ -49,16 +50,78 @@ impl Solver {
                     .map(|line| line[column_index])
                     .collect(),
             };
-            columns.push(column);
+            columns_part1.push(column);
         }
-        Solver { columns }
+
+        // part 2 input parsing
+
+        let byte_lines = input
+            .lines()
+            .map(|line| line.as_bytes())
+            .collect::<Vec<_>>();
+
+        let column_count = byte_lines[0].len();
+        let row_count = byte_lines.len();
+        let operator_line_index = row_count - 1;
+
+        let mut columns_part2 = Vec::<Column>::new();
+
+        let mut current_column: Option<Column> = None;
+        for column_index in 0..column_count {
+            if let op_sign @ b'+' | op_sign @ b'*' = byte_lines[operator_line_index][column_index] {
+                if let Some(current_column) = current_column.take() {
+                    columns_part2.push(current_column);
+                }
+                current_column = Some(Column {
+                    operator: match op_sign {
+                        b'+' => Operator::Add,
+                        b'*' => Operator::Multiply,
+                        _ => unreachable!(),
+                    },
+                    operands: Vec::new(),
+                });
+            }
+            let mut operand = 0;
+            for row in byte_lines.iter().take(row_count - 1) {
+                let symbol = row[column_index];
+                let n = match symbol {
+                    b'0' => 0,
+                    b'1' => 1,
+                    b'2' => 2,
+                    b'3' => 3,
+                    b'4' => 4,
+                    b'5' => 5,
+                    b'6' => 6,
+                    b'7' => 7,
+                    b'8' => 8,
+                    b'9' => 9,
+                    _ => continue,
+                };
+                operand = operand * 10 + n;
+            }
+            if operand != 0
+                && let Some(mut column) = current_column.take()
+            {
+                column.operands.push(operand);
+                current_column = Some(column);
+            }
+        }
+
+        if let Some(current_column) = current_column.take() {
+            columns_part2.push(current_column);
+        }
+
+        Solver {
+            columns_part1,
+            columns_part2,
+        }
     }
 }
 
 impl SolverBase for Solver {
     fn solve_part_one(&self) -> String {
         let mut sum = 0;
-        for column in &self.columns {
+        for column in &self.columns_part1 {
             let result = match column.operator {
                 Operator::Add => column.operands.iter().sum::<u64>(),
                 Operator::Multiply => column.operands.iter().product::<u64>(),
@@ -69,7 +132,15 @@ impl SolverBase for Solver {
     }
 
     fn solve_part_two(&self) -> String {
-        "".to_string()
+        let mut sum = 0;
+        for column in &self.columns_part2 {
+            let result = match column.operator {
+                Operator::Add => column.operands.iter().sum::<u64>(),
+                Operator::Multiply => column.operands.iter().product::<u64>(),
+            };
+            sum += result;
+        }
+        sum.to_string()
     }
 
     fn day_number(&self) -> usize {
