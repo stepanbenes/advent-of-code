0aba46f1b1a0cecf9e25db9a6f376ec94a0433e4
Štěpán Beneš <benes.stepan@gmail.com>
2025-12-14 11:13:52 +0100

    2025 Day07 part 1

---
+++ binaries/aoc_2025/src/day07.rs
@@ -0,0 +1,126 @@
+use solver::SolverBase;
+
+pub struct Solver {
+    grid: Vec<Vec<Location>>,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum Location {
+    Empty,
+    Start,
+    Splitter,
+    Beam,
+}
+
+impl Solver {
+    pub fn new(input: &'static str) -> Self {
+        let grid = input
+            .lines()
+            .map(|line| {
+                line.bytes()
+                    .map(|b| match b {
+                        b'.' => Location::Empty,
+                        b'S' => Location::Start,
+                        b'^' => Location::Splitter,
+                        b'|' => panic!("beam not expected in input"),
+                        _ => panic!("unknown location"),
+                    })
+                    .collect()
+            })
+            .collect();
+        Solver { grid }
+    }
+
+    pub fn tachion_beam_traverse(grid: &mut [Vec<Location>]) -> usize {
+        fn find_start(grid: &[Vec<Location>]) -> Option<(usize, usize)> {
+            for (i, row) in grid.iter().enumerate() {
+                for (j, location) in row.iter().enumerate() {
+                    if *location == Location::Start {
+                        return Some((i, j));
+                    }
+                }
+            }
+            None
+        }
+
+        fn dfs(grid: &mut [Vec<Location>], location: (usize, usize)) -> usize {
+            if location.0 >= grid.len() {
+                return 0; // left the grid
+            }
+            match grid[location.0][location.1] {
+                Location::Start => dfs(grid, (location.0 + 1, location.1)), // continue down
+                Location::Beam => 0,                                        // already occupied
+                Location::Splitter => {
+                    1 + dfs(grid, (location.0 + 1, location.1 - 1))
+                        + dfs(grid, (location.0 + 1, location.1 + 1))
+                } // split beam, add +1
+                Location::Empty => {
+                    grid[location.0][location.1] = Location::Beam; // mark as ocuppied
+                    dfs(grid, (location.0 + 1, location.1)) // continue down
+                }
+            }
+        }
+
+        dfs(grid, find_start(grid).unwrap())
+    }
+}
+
+impl SolverBase for Solver {
+    fn solve_part_one(&self) -> String {
+        let mut grid = self.grid.clone();
+        let split_count = Solver::tachion_beam_traverse(&mut grid);
+        split_count.to_string()
+    }
+
+    fn solve_part_two(&self) -> String {
+        "".to_string()
+    }
+
+    fn day_number(&self) -> usize {
+        7
+    }
+
+    fn description(&self) -> &'static str {
+        "Laboratories"
+    }
+}
+
+#[cfg(test)]
+mod part1_tests {
+    use super::*;
+
+    #[test]
+    fn test_1() {
+        let result = Solver::new(
+            r".......S.......
+...............
+.......^.......
+...............
+......^.^......
+...............
+.....^.^.^.....
+...............
+....^.^...^....
+...............
+...^.^...^.^...
+...............
+..^...^.....^..
+...............
+.^.^.^.^.^...^.
+...............",
+        )
+        .solve_part_one();
+        assert_eq!(result, "21");
+    }
+}
+
+// #[cfg(test)]
+// mod part2_tests {
+//     use super::*;
+
+//     #[test]
+//     fn test_1() {
+//         let result = Solver::new("abc").solve_part_two();
+//         assert_eq!(result, "0");
+//     }
+// }
