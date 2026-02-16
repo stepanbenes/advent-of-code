d82b304a6a8b448f2ea8cf41ab922dd9dad1f80d
Stepan Benes <benes.s@eplan.cz>
2026-02-16 09:45:16 +0100

    2025 day11 part 2 does not work

--- binaries/aoc_2025/src/day11.rs
+++ binaries/aoc_2025/src/day11.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 use solver::SolverBase;
 
@@ -27,6 +27,27 @@ impl Solver {
         }
         sum
     }
+
+    pub fn count_paths_that_visit(
+        &self,
+        from: &'static str,
+        to: &'static str,
+        must_visit: &[&'static str],
+    ) -> usize {
+        self.count_paths_that_contains_devices(from, to, &must_visit.to_vec())
+    }
+
+    fn count_paths_that_contains_devices(&self, from: &'static str, to: &'static str, must_visit: &Vec<&'static str>) -> usize {
+        if from == to {
+            return if must_visit.is_empty() { 1 } else { 0 };
+        }
+        let must_visit = if must_visit.contains(&from) { &must_visit.iter().filter(|x| **x != from).map(|x| *x).collect::<Vec<&'static str>>() } else { must_visit };
+        let mut sum = 0;
+        for output in self.devices.get(from).unwrap() {
+            sum += self.count_paths_that_contains_devices(output, to, must_visit);
+        }
+        sum
+    }
 }
 
 impl SolverBase for Solver {
@@ -36,7 +57,8 @@ impl SolverBase for Solver {
     }
 
     fn solve_part_two(&self) -> String {
-        "".to_string()
+        let count = self.count_paths_that_visit("svr", "out", &["dac", "fft"]);
+        count.to_string()
     }
 
     fn day_number(&self) -> usize {
@@ -71,13 +93,28 @@ iii: out",
     }
 }
 
-// #[cfg(test)]
-// mod part2_tests {
-//     use super::*;
+#[cfg(test)]
+mod part2_tests {
+    use super::*;
 
-//     #[test]
-//     fn test_1() {
-//         let result = Solver::new("abc").solve_part_two();
-//         assert_eq!(result, "0");
-//     }
-// }
+    #[test]
+    fn test_1() {
+        let result = Solver::new(
+            r"svr: aaa bbb
+aaa: fft
+fft: ccc
+bbb: tty
+tty: ccc
+ccc: ddd eee
+ddd: hub
+hub: fff
+eee: dac
+dac: fff
+fff: ggg hhh
+ggg: out
+hhh: out",
+        )
+        .solve_part_two();
+        assert_eq!(result, "2");
+    }
+}
